340bc217cf7753376d4baf88954bdb4dcce88587
scharnk <50645407+scharnk@users.noreply.github.com>
2022-01-21 04:48:52 -0800

    Update madam_marie_app.py

--- madam_marie_app.py
+++ madam_marie_app.py
@@ -81,7 +81,7 @@ with dataset:
 #     st.bokeh_chart(p, use_container_width=True)
 
 
-col1, col2, col3 = st.columns()
+col1, col2, col3 = st.columns([1, 1, 1])
 # col1, col2, col3 = st.beta_columns((1,1,0.5))) also works
 
 with col1:
