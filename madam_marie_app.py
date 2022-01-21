42e788452439ebe26fca41ed78487ca21edbe1a6
scharnk <50645407+scharnk@users.noreply.github.com>
2022-01-21 05:16:21 -0800

    selectboxes in sidebar

--- madam_marie_app.py
+++ madam_marie_app.py
@@ -27,12 +27,21 @@ with header:
     st.title('Fortune Teller Tarot Reader')
     img = Image.open("banner_image.jpg")
     st.image(img)
-    st.text('Data scraped from BiddyTarot.com')
-    st.text('')
-    st.text('App deployed using Heroku')
+    st.text('-- Data scraped from BiddyTarot.com')
+    # st.text('')
+    st.text('-- App deployed using Heroku')
 
 with sidebar:
     st.title('Select cards')
+    add_selectbox = st.selectbox(
+        "How would you like to be contacted?",
+        ("Email", "Home phone", "Mobile phone"))
+
+    option = st.selectbox(
+         'How would you like to be contacted?',
+         ('Email', 'Home phone', 'Mobile phone'))
+
+
 
 # with test:
 #     st.title('Select Spread')
@@ -46,7 +55,7 @@ with sidebar:
 #     YEAR = st.selectbox('YEAR:', ['2021','2020','2019','2018','2017','2016','2015'])
 #     monthh = st.selectbox('MONTH:', options1, format_func=lambda x: display1[x])
 #     MONTH = monthh+1
-
+    st.selectbox(label='Card 1', options1 = ('Major','Minor'))
     # st.selectbox(label='Card 1', options1 = display1)
     # st.selectbox(label='Card 2', options2 = display1)
     # st.selectbox(label='Card 3', options3 = display1)
